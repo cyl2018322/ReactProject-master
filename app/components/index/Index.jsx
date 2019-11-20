6250c5a28db5e20047a79400b627563361754507
cyl2018322 <15811547783@163.com>
2019-11-21 00:59:01 +0800

    陈艳丽test

--- app/components/index/Index.jsx
+++ app/components/index/Index.jsx
@@ -14,7 +14,7 @@ export default class Index extends React.Component {
                 <li className="bottom"><Link to="/productList"><i className="index_icon3"></i>产品列表</Link></li>
                 <li className="bottom"><Link to="/assets"><i className="index_icon4"></i>资产明细</Link></li>
                 <li className="bottom"><Link to="/transactionRecord"><i className="index_icon5"></i>交易记录</Link></li>
-                <li className="bottom"><Link to="/invite"><i className="index_icon6"></i>邀请好友</Link></li>
+                <li className="bottom"><Link to="/invite"><i className="index_icon6"></i>邀请好友分公司电饭锅</Link></li>
                 <li className="bottom"><Link to="/personalSetting"><i className="index_icon7"></i>个人设置</Link></li>
                 <li className="bottom"><Link to="/aboutUs"><i className="index_icon8"></i>关于我们</Link></li>
             </ul>
